6ac99d60587e67c57d2ba48203cb78c926bd230f
iostreamer-X <iostreamer007@gmail.com>
2019-06-10 02:20:35 +0530

    Added user, config and database services

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
-@Module({})
+@Module({
+  imports: [UserModule, DatabaseModule, ConfigModule]
+})
 export class MainModule {}
