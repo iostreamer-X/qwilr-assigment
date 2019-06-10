9086694347b36605d1b1b14df1c4e6c2cd2959e0
iostreamer-X <iostreamer007@gmail.com>
2019-06-11 02:07:15 +0530

    Added login support

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
 export function setupRoutes(instance) {
     instance.get('/', function (req, res) {
 		res.render('index', { title: 'Hey', message: 'Hello there!' });
+    });
+    
+    instance.get('/login', function (req, res) {
+		res.render('login', { title: 'Hey', message: 'Hello there!' });
 	});
 }
