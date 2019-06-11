486aa533e13e20ec406f35c3e93041c9693caa4e
iostreamer-X <iostreamer007@gmail.com>
2019-06-11 19:54:11 +0530

    Added stock buy support

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,44 @@
 $(document)
 .ready(function() {
-	$('#addBalanceButton').click(async function (event) {
+    handleAddBalance();
+    hideCart();
+    handleAddToCart();
+    handleStockQuantity();
+    handleBuy();
+
+    $('#buyError').hide();
+    $('#buySuccess').hide();
+});
+
+function viewCart(name, price) {
+    $('#cart').show();
+    $('#stockBuyName').text(name);
+    $('#stockBuyPrice').text(price);
+}
+
+function hideCart() {
+    $('#cart').hide();
+}
+
+function handleStockQuantity() {
+    $('#addStockQuantity').click(function (params) {
+        const quantity = Number($('#stockQuantity').val());
+        $('#stockQuantity').val(quantity + 1); 
+    });
+}
+
+function handleAddToCart() {
+    $('#addToCartButton').click(function (event) {
+        const stockName = $('#addToCartButton').attr('stockName');
+        const stockPrice = $('#addToCartButton').attr('stockPrice');
+        $('#buyButton').attr('stockName', stockName);
+        $('#buyButton').attr('stockPrice', stockPrice);
+        viewCart(stockName, stockPrice);
+    });
+}
+
+function handleAddBalance() {
+    $('#addBalanceButton').click(async function (event) {
 		const amount = Number($('#amount').val());
 		try {
             await request('/balance/add', {
@@ -9,16 +47,47 @@ $(document)
                 }),
                 type: 'POST'
             });
-            const { data: {balance} } = await request('/balance/get', {
-                type: 'GET'
-            });
-            $('#balance').text(`Current Balance: ${balance}`);
-            $('#amount').val('');
+            await renderUpdatedBalance();
         } catch (error) {
             console.log(error);
         }
 	});
-});
+}
+
+async function renderUpdatedBalance() {
+    const { data: {balance} } = await request('/balance/get', {
+        type: 'GET'
+    });
+    $('#balance').text(`Current Balance: ${balance}`);
+    $('#amount').val('');
+}
+
+function handleBuy() {
+    $('#buyButton').click(async function (params) {
+        const stockName = $('#buyButton').attr('stockName');
+        const quantity = Number($('#stockQuantity').val());
+        try {
+            await request('/balance/stocks/buy', {
+                data: JSON.stringify({
+                    name: stockName,
+                    quantity
+                }),
+                type: 'POST'
+            });
+            await renderUpdatedBalance();
+            $('#buyError').hide();
+            $('#buySuccess').show();
+            $('#stockQuantity').val('');
+            setTimeout(function () {
+                $('#buySuccess').hide();
+            }, 1500)
+        } catch (error) {
+            console.log(error);
+            $('#buyError').show();
+            $('#buyErrorMessage').text(error.responseJSON.error.message);
+        }  
+    })
+}
 
 function request(url, options) {
     return new Promise((resolve, reject) => {
